2bcf4564c0bce3386c8b2aa24563859209188cb5
be-marc <marcbecker@posteo.de>
2023-12-12 14:54:52 +0100

    feat: add weka learner

--- tests/testthat/test_LearnerAuto.R
+++ tests/testthat/test_LearnerAuto.R
@@ -1,45 +1,27 @@
-test_that("classification graph is constructed", {
-  learner_ids = c("J48", "DecisionTable", "KStar", "LMT", "PART", "SMO","BayesNet", "JRip", "SimpleLogistic",
-    "VotedPerceptron", "SGD", "Logistic", "OneR", "MultilayerPerceptron", "REPTree", "IBk", "RandomForestWEKA",
-    "RandomTree")
-
-  graph = get_branch_pipeline("classif", learner_ids)
-  expect_class(graph, "Graph")
-})
-
-test_that("classification search_space is constructed", {
-  learner_ids = c("J48", "DecisionTable", "KStar", "LMT", "PART", "SMO", "BayesNet", "JRip", "SimpleLogistic",
-    "VotedPerceptron", "SGD", "Logistic", "OneR", "MultilayerPerceptron", "REPTree", "IBk", "RandomForestWEKA",
-    "RandomTree")
-
-  search_space = get_search_space("classif", learner_ids, tuning_space_classif_autoweka)
-  expect_class(search_space, "ParamSet")
-})
-
-test_that("LearnerClassifAutoWEKA train works", {
+test_that("LearnerClassifAuto train works", {
   task = tsk("sonar")
   resampling = rsmp("holdout")
   measure = msr("classif.ce")
   terminator = trm("run_time", secs = 10)
-  learner = LearnerClassifAutoWEKA$new(
+  learner = LearnerClassifAuto$new(
     resampling = resampling,
     measure = measure,
     terminator = terminator)
 
-  expect_names(learner$packages, must.include = "RWeka")
+  expect_names(learner$packages, must.include = "xgboost")
 
-  expect_class(learner$train(task), "LearnerClassifAutoWEKA")
+  expect_class(learner$train(task), "LearnerClassifAuto")
   expect_class(learner$model, "AutoTuner")
 
   expect_prediction(learner$predict(task))
 })
 
-test_that("LearnerClassifAutoWEKA resample works", {
+test_that("LearnerClassifAuto resample works", {
   task = tsk("sonar")
   resampling = rsmp("holdout")
   measure = msr("classif.ce")
   terminator = trm("run_time", secs = 10)
-  learner = LearnerClassifAutoWEKA$new(
+  learner = LearnerClassifAuto$new(
     resampling = resampling,
     measure = measure,
     terminator = terminator)
@@ -47,19 +29,19 @@ test_that("LearnerClassifAutoWEKA resample works", {
   expect_resample_result(resample(task, learner, rsmp("holdout")))
 })
 
-test_that("LearnerClassifAutoWEKA train works with parallelization", {
+test_that("LearnerClassifAuto train works with parallelization", {
   future::plan("multisession", workers = 2)
 
   task = tsk("sonar")
   resampling = rsmp("holdout")
   measure = msr("classif.ce")
   terminator = trm("run_time", secs = 10)
-  learner = LearnerClassifAutoWEKA$new(
+  learner = LearnerClassifAuto$new(
     resampling = resampling,
     measure = measure,
     terminator = terminator)
 
-  expect_class(learner$train(task), "LearnerClassifAutoWEKA")
+  expect_class(learner$train(task), "LearnerClassifAuto")
   expect_class(learner$model, "AutoTuner")
 })
 
@@ -68,13 +50,13 @@ test_that("callback timeout works", {
   resampling = rsmp("holdout")
   measure = msr("classif.ce")
   terminator = trm("run_time", secs = 20)
-  learner = LearnerClassifAutoWEKA$new(
+  learner = LearnerClassifAuto$new(
     resampling = resampling,
     measure = measure,
     terminator = terminator,
     callbacks = clbk("mlr3tuning.timeout", time_limit = 20))
 
-  expect_class(learner$train(task), "LearnerClassifAutoWEKA")
+  expect_class(learner$train(task), "LearnerClassifAuto")
   expect_lte(learner$model$tuning_instance$archive$benchmark_result$learners$learner[[1]]$timeout["train"], 20)
 })
 
@@ -84,12 +66,12 @@ test_that("callback timeout works", {
   resampling = rsmp("holdout")
   measure = msr("classif.ce")
   terminator = trm("run_time", secs = 20)
-  learner = LearnerClassifAutoWEKA$new(
+  learner = LearnerClassifAuto$new(
     resampling = resampling,
     measure = measure,
     terminator = terminator,
     callbacks = clbk("mlr3tuning.timeout", time_limit = 20, max_time_limit = 5))
 
-  expect_class(learner$train(task), "LearnerClassifAutoWEKA")
+  expect_class(learner$train(task), "LearnerClassifAuto")
   expect_lte(learner$model$tuning_instance$archive$benchmark_result$learners$learner[[1]]$timeout["train"], 5)
 })
