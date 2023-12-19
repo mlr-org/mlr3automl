2705a3a75f110e4e4ed10a91a2eef90b0a9228c4
be-marc <marcbecker@posteo.de>
2023-12-19 09:33:11 +0100

    tests: search spaces can be set

--- tests/testthat/test_LearnerAutoWEKA.R
+++ tests/testthat/test_LearnerAutoWEKA.R
@@ -20,6 +20,47 @@ test_that("classification search_space is constructed", {
   expect_class(search_space, "ParamSet")
 })
 
+test_that("classification search space can be set", {
+  skip_on_ci()
+
+  learner_ids = c("J48", "DecisionTable", "KStar", "LMT", "PART", "SMO", "BayesNet", "JRip", "SimpleLogistic",
+    "VotedPerceptron", "SGD", "Logistic", "OneR", "MultilayerPerceptron", "REPTree", "IBk", "RandomForestWEKA",
+    "RandomTree")
+
+  graph = get_branch_pipeline("classif", learner_ids)
+  learner = as_learner(graph)
+  search_space = get_search_space("classif", learner_ids, tuning_space_classif_autoweka)
+
+  design = generate_design_random(search_space,  1000)$data
+  xss = transform_xdt_to_xss(design, search_space)
+
+  iwalk(xss, function(xs, i) {
+    learner$param_set$values = list()
+    expect_class(learner$param_set$set_values(.values = xs), "ParamSet")
+  })
+})
+
+test_that("regression search space can be set", {
+  skip_on_ci()
+
+  learner_ids = c("DecisionTable", "M5P", "KStar", "LinearRegression", "SGD",
+    "MultilayerPerceptron", "REPTree", "IBk", "M5Rules", "RandomForestWEKA", "RandomTree",
+    "GaussianProcesses","SMOreg")
+
+  graph = get_branch_pipeline("regr", learner_ids)
+  learner = as_learner(graph)
+  search_space = get_search_space("regr", learner_ids, tuning_space_regr_autoweka)
+
+  design = generate_design_random(search_space,  1000)$data
+  xss = transform_xdt_to_xss(design, search_space)
+
+  iwalk(xss, function(xs, i) {
+    print(i)
+    learner$param_set$values = list()
+    expect_class(learner$param_set$set_values(.values = xs), "ParamSet")
+  })
+})
+
 test_that("LearnerClassifAutoWEKA train works", {
   skip_on_ci()
 
@@ -35,6 +76,26 @@ test_that("LearnerClassifAutoWEKA train works", {
   expect_names(learner$packages, must.include = "RWeka")
 
   expect_class(learner$train(task), "LearnerClassifAutoWEKA")
+  expect_class(learner$model, "AutoTuner")
+
+  expect_prediction(learner$predict(task))
+})
+
+test_that("LearnerRegrAutoWEKA train works", {
+  skip_on_ci()
+
+  task = tsk("mtcars")
+  resampling = rsmp("holdout")
+  measure = msr("regr.mse")
+  terminator = trm("run_time", secs = 10)
+  learner = LearnerRegrAutoWEKA$new(
+    resampling = resampling,
+    measure = measure,
+    terminator = terminator)
+
+  expect_names(learner$packages, must.include = "RWeka")
+
+  expect_class(learner$train(task), "LearnerRegrAutoWEKA")
   expect_class(learner$model, "AutoTuner")
 
   expect_prediction(learner$predict(task))
