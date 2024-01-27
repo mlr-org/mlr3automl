62614565d3082bb74b818649bed410e72df64b12
be-marc <marcbecker@posteo.de>
2024-01-27 17:46:01 +0100

    test: parameter

--- tests/testthat/test_LearnerAuto.R
+++ tests/testthat/test_LearnerAuto.R
@@ -40,6 +40,29 @@ test_that("LearnerClassifAuto train works with early stopping eval metric", {
   expect_prediction(learner$predict(task))
 })
 
+test_that("LearnerClassifAuto train works with early_stopping_rounds parameter", {
+  task = tsk("pima")
+  splits = partition(task, ratio = 0.9, stratify = TRUE)
+  task$set_row_roles(splits$test, "holdout")
+
+  resampling = rsmp("holdout")
+  measure = msr("classif.ce")
+  terminator = trm("run_time", secs = 10)
+  learner = LearnerClassifAuto$new(
+    resampling = resampling,
+    measure = measure,
+    terminator = terminator,
+    early_stopping_rounds = 5,
+    eval_metric = "auc")
+
+  expect_class(learner$train(task), "LearnerClassifAuto")
+  expect_class(learner$model$graph_learner, "GraphLearner")
+  expect_class(learner$model$instance, "TuningInstanceSingleCrit")
+  expect_class(learner$model$graph_learner$state$model$xgboost$model, "xgb.Booster")
+  expect_true(grepl("auc", learner$model$graph_learner$model$xgboost$model$best_msg))
+  expect_prediction(learner$predict(task))
+})
+
 test_that("LearnerClassifAuto works with resample", {
   task = tsk("penguins")
   splits = partition(task, ratio = 0.9, stratify = TRUE)
