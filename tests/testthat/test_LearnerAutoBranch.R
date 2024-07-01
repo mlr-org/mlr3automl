d46d7a6fe3a0e653a45bef06973b26e46b402ee4
be-marc <marcbecker@posteo.de>
2024-07-01 13:50:11 +0200

    tests: activate tests

--- tests/testthat/test_LearnerAutoBranch.R
+++ tests/testthat/test_LearnerAutoBranch.R
@@ -112,94 +112,94 @@ test_that("svm works", {
   expect_equal(learner$model$instance$result$branch.selection, "svm")
 })
 
-# test_that("xgboost works", {
-#   rush_plan(n_workers = 2)
-#   lgr::get_logger("mlr3automl")$set_threshold("debug")
+test_that("xgboost works", {
+  rush_plan(n_workers = 2)
+  lgr::get_logger("mlr3automl")$set_threshold("debug")
 
-#   task = tsk("penguins")
-#   learner = lrn("classif.automl_branch",
-#     learner_ids = "xgboost",
-#     xgboost_eval_metric = "mlogloss",
-#     small_data_size = 100,
-#     measure = msr("classif.ce"),
-#     terminator = trm("evals", n_evals = 6)
-#   )
+  task = tsk("penguins")
+  learner = lrn("classif.automl_branch",
+    learner_ids = "xgboost",
+    xgboost_eval_metric = "mlogloss",
+    small_data_size = 100,
+    measure = msr("classif.ce"),
+    terminator = trm("evals", n_evals = 6)
+  )
 
-#   expect_class(learner$train(task), "LearnerClassifAutoBranch")
-#   expect_equal(learner$model$instance$result$branch.selection, "xgboost")
-#   expect_numeric(learner$model$instance$archive$data$max_nrounds, lower = 1)
-# })
+  expect_class(learner$train(task), "LearnerClassifAutoBranch")
+  expect_equal(learner$model$instance$result$branch.selection, "xgboost")
+  expect_numeric(learner$model$instance$archive$data$max_nrounds, lower = 1)
+})
 
-# test_that("catboost works", {
-#   rush_plan(n_workers = 2)
-#   lgr::get_logger("mlr3automl")$set_threshold("debug")
+test_that("catboost works", {
+  rush_plan(n_workers = 2)
+  lgr::get_logger("mlr3automl")$set_threshold("debug")
 
-#   task = tsk("penguins")
-#   learner = lrn("classif.automl_branch",
-#     learner_ids = "catboost",
-#     catboost_eval_metric = "MultiClass",
-#     small_data_size = 100,
-#     measure = msr("classif.ce"),
-#     terminator = trm("evals", n_evals = 6)
-#   )
+  task = tsk("penguins")
+  learner = lrn("classif.automl_branch",
+    learner_ids = "catboost",
+    catboost_eval_metric = "MultiClass",
+    small_data_size = 100,
+    measure = msr("classif.ce"),
+    terminator = trm("evals", n_evals = 6)
+  )
 
-#   expect_class(learner$train(task), "LearnerClassifAutoBranch")
-#   expect_equal(learner$model$instance$result$branch.selection, "catboost")
-#   expect_numeric(learner$model$instance$archive$data$max_nrounds, lower = 1)
-# })
+  expect_class(learner$train(task), "LearnerClassifAutoBranch")
+  expect_equal(learner$model$instance$result$branch.selection, "catboost")
+  expect_numeric(learner$model$instance$archive$data$max_nrounds, lower = 1)
+})
 
-# test_that("extra_trees works", {
-#   rush_plan(n_workers = 2)
-#   lgr::get_logger("mlr3automl")$set_threshold("debug")
+test_that("extra_trees works", {
+  rush_plan(n_workers = 2)
+  lgr::get_logger("mlr3automl")$set_threshold("debug")
 
-#   task = tsk("penguins")
-#   learner = lrn("classif.automl_branch",
-#     learner_ids = "extra_trees",
-#     small_data_size = 100,
-#     measure = msr("classif.ce"),
-#     terminator = trm("evals", n_evals = 6)
-#   )
+  task = tsk("penguins")
+  learner = lrn("classif.automl_branch",
+    learner_ids = "extra_trees",
+    small_data_size = 100,
+    measure = msr("classif.ce"),
+    terminator = trm("evals", n_evals = 6)
+  )
 
-#   expect_class(learner$train(task), "LearnerClassifAutoBranch")
-#   expect_equal(learner$model$instance$result$branch.selection, "extra_trees")
-# })
+  expect_class(learner$train(task), "LearnerClassifAutoBranch")
+  expect_equal(learner$model$instance$result$branch.selection, "extra_trees")
+})
 
-# test_that("lightgbm works", {
-#   rush_plan(n_workers = 2)
-#   lgr::get_logger("mlr3automl")$set_threshold("debug")
+test_that("lightgbm works", {
+  rush_plan(n_workers = 2)
+  lgr::get_logger("mlr3automl")$set_threshold("debug")
 
-#   task = tsk("penguins")
-#   learner = lrn("classif.automl_branch",
-#     learner_ids = "lightgbm",
-#     lightgbm_eval_metric = "multi_logloss",
-#     small_data_size = 100,
-#     measure = msr("classif.ce"),
-#     terminator = trm("evals", n_evals = 6)
-#   )
+  task = tsk("penguins")
+  learner = lrn("classif.automl_branch",
+    learner_ids = "lightgbm",
+    lightgbm_eval_metric = "multi_logloss",
+    small_data_size = 100,
+    measure = msr("classif.ce"),
+    terminator = trm("evals", n_evals = 6)
+  )
 
-#   expect_class(learner$train(task), "LearnerClassifAutoBranch")
-#   expect_equal(learner$model$instance$result$branch.selection, "lightgbm")
-# })
+  expect_class(learner$train(task), "LearnerClassifAutoBranch")
+  expect_equal(learner$model$instance$result$branch.selection, "lightgbm")
+})
 
-# test_that("xgboost, catboost and lightgbm work", {
-#   rush_plan(n_workers = 2)
-#   lgr::get_logger("mlr3automl")$set_threshold("debug")
+test_that("xgboost, catboost and lightgbm work", {
+  rush_plan(n_workers = 2)
+  lgr::get_logger("mlr3automl")$set_threshold("debug")
 
-#   task = tsk("penguins")
-#   learner = lrn("classif.automl_branch",
-#     learner_ids = c("xgboost", "catboost", "lightgbm"),
-#     catboost_eval_metric = "MultiClass",
-#     lightgbm_eval_metric = "multi_logloss",
-#     xgboost_eval_metric = "mlogloss",
-#     small_data_size = 100,
-#     lhs_size = 1,
-#     measure = msr("classif.ce"),
-#     terminator = trm("evals", n_evals = 20),
-#     callbacks = clbk("mlr3tuning.async_save_logs")
-#   )
+  task = tsk("penguins")
+  learner = lrn("classif.automl_branch",
+    learner_ids = c("xgboost", "catboost", "lightgbm"),
+    catboost_eval_metric = "MultiClass",
+    lightgbm_eval_metric = "multi_logloss",
+    xgboost_eval_metric = "mlogloss",
+    small_data_size = 100,
+    lhs_size = 1,
+    measure = msr("classif.ce"),
+    terminator = trm("evals", n_evals = 20),
+    callbacks = clbk("mlr3tuning.async_save_logs")
+  )
 
-#   expect_class(learner$train(task), "LearnerClassifAutoBranch")
-# })
+  expect_class(learner$train(task), "LearnerClassifAutoBranch")
+})
 
 test_that("all learner work", {
   rush_plan(n_workers = 2)
