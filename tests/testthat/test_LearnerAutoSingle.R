e2dce7751270501dd85eaabd7562d4feac2ce499
b-zhou <Baisu.Zhou@campus.lmu.de>
2024-07-19 10:36:42 +0200

    test: no branch selection

--- tests/testthat/test_LearnerAutoSingle.R
+++ tests/testthat/test_LearnerAutoSingle.R
@@ -1,3 +1,12 @@
+test_that("no branch selection is performed", {
+  learner = lrn("classif.automl_svm",
+    small_data_size = 100,
+    measure = msr("classif.ce"),
+    terminator = trm("evals", n_evals = 6)
+  )
+  expect_false("branch.selection" %in% learner$param_set$ids())
+})
+
 test_that("single svm works", {
   rush_plan(n_workers = 2)
   lgr::get_logger("mlr3automl")$set_threshold("debug")
