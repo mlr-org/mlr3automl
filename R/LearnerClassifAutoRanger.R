d8e947cf88a227a237c01050c8cfa221fc8e83ff
b-zhou <Baisu.Zhou@campus.lmu.de>
2024-07-28 15:40:32 +0200

    feat: classif single learners

---
+++ R/LearnerClassifAutoRanger.R
@@ -0,0 +1,40 @@
+#' @title Classification Ranger Auto Learner
+#'
+#' @description
+#' Classification auto learner.
+#'
+#' @template param_id
+#'
+#' @export
+LearnerClassifAutoRanger = R6Class("LearnerClassifAutoRanger",
+  inherit = LearnerClassifAuto,
+  public = list(
+
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    initialize = function(id = "classif.auto_ranger") {
+      super$initialize(id = id)
+
+      # reduce parameter set to the relevant parameters
+      private$.param_set = private$.param_set$subset(
+        c("learner_ids",
+        "learner_timeout",
+        "small_data_size",
+        "small_data_resampling",
+        "max_cardinality",
+        "resampling",
+        "terminator",
+        "measure",
+        "lhs_size",
+        "callbacks",
+        "store_benchmark_result")
+      )
+
+      self$param_set$set_values(learner_ids = "ranger")
+      self$packages = c("mlr3tuning", "mlr3learners", "mlr3pipelines", "mlr3mbo", "mlr3automl", "ranger")
+    }
+  )
+)
+
+#' @include aaa.R
+learners[["classif.auto_ranger"]] = LearnerClassifAutoRanger
