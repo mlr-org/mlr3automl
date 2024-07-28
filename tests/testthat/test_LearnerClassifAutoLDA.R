603604f0050683e62ddc6d36da58b372aba3825d
b-zhou <Baisu.Zhou@campus.lmu.de>
2024-07-28 15:44:51 +0200

    test: classif single learners

---
+++ tests/testthat/test_LearnerClassifAutoLDA.R
@@ -0,0 +1,25 @@
+test_that("LearnerClassifAutoLDA is initialized", {
+  learner = lrn("classif.auto_lda",
+    measure = msr("classif.ce"),
+    terminator = trm("evals", n_evals = 10))
+
+  expect_null(learner$graph)
+  expect_null(learner$tuning_space)
+})
+
+test_that("LearnerClassifAutoLDA is trained", {
+  rush_plan(n_workers = 2)
+  lgr::get_logger("mlr3automl")$set_threshold("debug")
+
+  task = tsk("penguins")
+  learner = lrn("classif.auto_lda",
+    small_data_size = 1,
+    resampling = rsmp("holdout"),
+    measure = msr("classif.ce"),
+    terminator = trm("evals", n_evals = 6)
+  )
+
+  expect_class(learner$train(task), "LearnerClassifAutoLDA")
+  expect_equal(learner$graph$param_set$values$branch.selection, "lda")
+  expect_equal(learner$model$instance$result$branch.selection, "lda")
+})
