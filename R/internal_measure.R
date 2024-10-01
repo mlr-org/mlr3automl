a5d2185d2bccdb49fe3fb55155ddd04062bf7431
b-zhou <Baisu.Zhou@campus.lmu.de>
2024-10-01 14:29:36 +0200

    fix: internal measure lightgbm multiclass

--- R/internal_measure.R
+++ R/internal_measure.R
@@ -1,47 +1,3 @@
-#' @title Internal Measure XGBoost
-#'
-#' @description
-#' Function to get the internal xgboost measure for a given [mlr3::Task] and [mlr3::Measure].
-#' For example, the measure "classif.auc" will return "auc" for a binary classification task.
-#'
-#' @param measure [mlr3::Measure]\cr
-#' Measure to get the internal measure for.
-#' @param task [mlr3::Task]\cr
-#' Task to get the internal measure for.
-#'
-#' @export
-#' @examples
-#' internal_measure_xgboost(msr("classif.auc"), tsk("pima"))
-internal_measure_xgboost = function(measure, task) {
-  id = measure$id
-
-  metric = if (task$task_type == "regr") {
-    switch(id,
-      "regr.rmse" = "rmse",
-      "regr.rmsle" = "rmsle",
-      "regr.mae" = "mae",
-      "regr.mape" = "mape",
-      "regr.logloss" = "logloss",
-      NULL
-    )
-  } else if ("twoclass" %in% task$properties) {
-    switch(id,
-      "classif.ce" = "error",
-      "classif.acc" = "error",
-      "classif.auc" = "auc",
-      NULL
-    )
-  } else if ("multiclass" %in% task$properties) {
-    switch(id,
-      "classif.ce" = "merror",
-      "classif.acc" = "merror",
-      NULL
-    )
-  }
-
-  return(metric %??% NA_character_)
-}
-
 #' @title Internal Measure Catboost
 #'
 #' @description
@@ -95,7 +51,7 @@ internal_measure_catboost = function(measure, task) {
 #' @title Internal Measure LightGBM
 #'
 #' @description
-#' Function to get the internal xgboost measure for a given [mlr3::Task] and [mlr3::Measure].
+#' Function to get the internal lightgbm measure for a given [mlr3::Task] and [mlr3::Measure].
 #' For example, the measure "classif.auc" will return "auc" for a binary classification task.
 #'
 #' @param measure [mlr3::Measure]\cr
@@ -129,8 +85,56 @@ internal_measure_lightgbm = function(measure, task) {
     switch(id,
       "classif.ce" = "multi_error",
       "classif.acc" = "multi_error",
-      "classif.auc" = "auc_mu",
+      "classif.mauc_mu" = "auc_mu",
       "classif.logloss" = "multi_logloss",
+      NULL
+    )
+  }
+
+  return(metric %??% NA_character_)
+}
+
+
+#' @title Internal Measure XGBoost
+#'
+#' @description
+#' Function to get the internal xgboost measure for a given [mlr3::Task] and [mlr3::Measure].
+#' For example, the measure "classif.auc" will return "auc" for a binary classification task.
+#'
+#' @param measure [mlr3::Measure]\cr
+#' Measure to get the internal measure for.
+#' @param task [mlr3::Task]\cr
+#' Task to get the internal measure for.
+#'
+#' @export
+#' @examples
+#' internal_measure_xgboost(msr("classif.auc"), tsk("pima"))
+internal_measure_xgboost = function(measure, task) {
+  id = measure$id
+
+  metric = if (task$task_type == "regr") {
+    switch(id,
+      "regr.mse" = "mse",
+      "regr.rmse" = "rmse",
+      "regr.mae" = "mae",
+      "regr.mape" = "mape",
+      NULL
+    )
+  } else if ("twoclass" %in% task$properties) {
+    switch(id,
+      "classif.ce" = "error",
+      "classif.acc" = "error",
+      "classif.auc" = "auc",
+      "classif.prauc" = "aucpr",
+      "classif.logloss" = "logloss",
+      NULL
+    )
+  } else if ("multiclass" %in% task$properties) {
+    switch(id,
+      "classif.ce" = "merror",
+      "classif.acc" = "merror",
+      "classif.mauc_aunp" = "auc",
+      "classif.logloss" = "mlogloss",
       NULL
     )
   }
