dec1f9c56f1273dc98c06deb7024ff6ea8602db2
b-zhou <Baisu.Zhou@campus.lmu.de>
2024-10-07 19:43:37 +0200

    test: cot & mp fixes

--- tests/testthat/test_visualization.R
+++ tests/testthat/test_visualization.R
@@ -1,8 +1,5 @@
 skip_on_cran()
 skip_if_not_installed("rush")
-flush_redis()
-
-rush_plan(n_workers = 2)
 skip_if_not_installed(c("glmnet", "kknn", "ranger", "e1071"))
 
 # cost over time
@@ -10,6 +7,9 @@ test_that("cost over time works", {
   task = tsk("penguins")
 
   set.seed(1453)
+  flush_redis()
+  rush_plan(n_workers = 2)
+
   learner = lrn("classif.auto_ranger",
     small_data_size = 1,
     resampling = rsmp("holdout"),
@@ -19,8 +19,8 @@ test_that("cost over time works", {
   learner$train(task)
 
   vdiffr::expect_doppelganger("cot-config-id", cost_over_time(learner$instance))
-  vdiffr::expect_doppelganger("cot-timestamp-x", cost_over_time(learner$instance, time = "timestamp_x"))
-  vdiffr::expect_doppelganger("cot-timestamp-y", cost_over_time(learner$instance, time = "timestamp_y"))
+  vdiffr::expect_doppelganger("cot-timestamp-xs", cost_over_time(learner$instance, time = "timestamp_xs"))
+  vdiffr::expect_doppelganger("cot-timestamp-ys", cost_over_time(learner$instance, time = "timestamp_ys"))
 })
 
 
@@ -30,6 +30,9 @@ test_that("marginal plot works", {
 
   # numeric vs numeric
   set.seed(1453)
+  flush_redis()
+  rush_plan(n_workers = 2)
+
   learner_glmnet = lrn("classif.auto_glmnet",
     small_data_size = 1,
     resampling = rsmp("holdout"),
@@ -48,6 +51,9 @@ test_that("marginal plot works", {
 
   # numeric vs factor
   set.seed(1453)
+  flush_redis()
+  rush_plan(n_workers = 2)
+  
   learner_kknn = lrn("classif.auto_kknn",
     small_data_size = 1,
     resampling = rsmp("holdout"),
@@ -66,6 +72,9 @@ test_that("marginal plot works", {
 
   # numeric vs logical
   set.seed(1453)
+  flush_redis()
+  rush_plan(n_workers = 2)
+  
   learner_ranger = lrn("classif.auto_ranger",
     small_data_size = 1,
     resampling = rsmp("holdout"),
@@ -75,11 +84,11 @@ test_that("marginal plot works", {
   learner_ranger$train(task)
   vdiffr::expect_doppelganger(
     "mp-numeric-logical",
-    marginal_plot(learner$instance, x = "ranger.num.trees", y = "ranger.replace")
+    marginal_plot(learner_ranger$instance, x = "ranger.num.trees", y = "ranger.replace")
   )
   vdiffr::expect_doppelganger(
     "mp-logical-numeric",
-    marginal_plot(learner$instance, x = "ranger.replace", y = "ranger.num.trees")
+    marginal_plot(learner_ranger$instance, x = "ranger.replace", y = "ranger.num.trees")
   )
 })
 
@@ -87,13 +96,17 @@ test_that("marginal plot throws error if params on different branches", {
   task = tsk("penguins")
 
   set.seed(1453)
+  flush_redis()
+  rush_plan(n_workers = 2)
+  
   learner = lrn("classif.auto",
-    learner_ids = c("kknn", "svm")
+    learner_ids = c("kknn", "svm"),
     small_data_size = 1,
     resampling = rsmp("holdout"),
     measure = msr("classif.ce"),
     terminator = trm("evals", n_evals = 6)
   )
+  learner$train(task)
   expect_error(
     marginal_plot(learner$instance, x = "kknn.distance", y = "svm.cost")
   )
@@ -103,6 +116,9 @@ test_that("marginal plot handles dependence", {
   task = tsk("penguins")
 
   set.seed(1453)
+  flush_redis()
+  rush_plan(n_workers = 2)
+  
   learner_svm = lrn("classif.auto_svm",
     small_data_size = 1,
     resampling = rsmp("holdout"),
