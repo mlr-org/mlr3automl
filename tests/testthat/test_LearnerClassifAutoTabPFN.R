0bae56287ca1d3af7e73e814552f1f66b791c4bd
b-zhou <Baisu.Zhou@campus.lmu.de>
2025-07-30 16:31:48 +0200

    feat & test: add single learners

---
+++ tests/testthat/test_LearnerClassifAutoTabPFN.R
@@ -0,0 +1,29 @@
+test_that("LearnerClassifAutoTabPFN is initialized", {
+  learner = lrn("classif.auto_tabpfn",
+    measure = msr("classif.ce"),
+    terminator = trm("evals", n_evals = 10))
+
+  expect_class(learner$graph, "Graph")
+  expect_list(learner$tuning_space)
+})
+
+test_that("LearnerClassifAutoTabPFN is trained", {
+  skip_on_cran()
+  skip_if_not_installed("rush")
+  flush_redis()
+
+  rush_plan(n_workers = 2)
+
+
+  task = tsk("penguins")
+  learner = lrn("classif.auto_tabpfn",
+    small_data_size = 1,
+    resampling = rsmp("holdout"),
+    measure = msr("classif.ce"),
+    terminator = trm("evals", n_evals = 6)
+  )
+
+  expect_class(learner$train(task), "LearnerClassifAutoTabPFN")
+  expect_equal(learner$graph$param_set$values$branch.selection, "tabpfn")
+  expect_equal(learner$model$instance$result$branch.selection, "tabpfn")
+})
---
+++ tests/testthat/test_LearnerRegrAutoTabPFN.R
@@ -0,0 +1,29 @@
+test_that("LearnerRegrAutoTabPFN is initialized", {
+  learner = lrn("regr.auto_tabpfn",
+    measure = msr("regr.rmse"),
+    terminator = trm("evals", n_evals = 10))
+
+  expect_class(learner$graph, "Graph")
+  expect_list(learner$tuning_space)
+})
+
+test_that("LearnerRegrAutoTabPFN is trained", {
+  skip_on_cran()
+  skip_if_not_installed("rush")
+  flush_redis()
+
+  rush_plan(n_workers = 2)
+
+
+  task = tsk("mtcars")
+  learner = lrn("regr.auto_tabpfn",
+    small_data_size = 1,
+    resampling = rsmp("holdout"),
+    measure = msr("regr.rmse"),
+    terminator = trm("evals", n_evals = 6)
+  )
+
+  expect_class(learner$train(task), "LearnerRegrAutoTabPFN")
+  expect_equal(learner$graph$param_set$values$branch.selection, "tabpfn")
+  expect_equal(learner$model$instance$result$branch.selection, "tabpfn")
+})
