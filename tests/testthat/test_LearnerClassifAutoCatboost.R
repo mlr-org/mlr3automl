8e79c3b4728b4ab31cca981c4304ad5cbc6df183
be-marc <marcbecker@posteo.de>
2025-09-12 18:54:21 +0200

    ...

--- tests/testthat/test_LearnerClassifAutoCatboost.R
+++ tests/testthat/test_LearnerClassifAutoCatboost.R
@@ -1,4 +1,6 @@
 test_that("LearnerClassifAutoCatboost is initialized", {
+  skip_if_not_installed("catboost")
+
   learner = lrn("classif.auto_catboost",
     measure = msr("classif.ce"),
     terminator = trm("evals", n_evals = 10))
@@ -9,11 +11,11 @@ test_that("LearnerClassifAutoCatboost is initialized", {
 
 test_that("LearnerClassifAutoCatboost is trained", {
   skip_on_cran()
+  skip_if_not_installed("catboost")
   skip_if_not_installed("rush")
   flush_redis()
 
   rush_plan(n_workers = 2)
-
 
   task = tsk("penguins")
   learner = lrn("classif.auto_catboost",
@@ -21,7 +23,10 @@ test_that("LearnerClassifAutoCatboost is trained", {
     small_data_size = 1,
     resampling = rsmp("holdout"),
     measure = msr("classif.ce"),
-    terminator = trm("evals", n_evals = 6)
+    terminator = trm("evals", n_evals = 6),
+    lhs_size = 1,
+    encapsulate_learner = FALSE,
+    encapsulate_mbo = FALSE
   )
 
   expect_class(learner$train(task), "LearnerClassifAutoCatboost")
@@ -31,11 +36,11 @@ test_that("LearnerClassifAutoCatboost is trained", {
 
 test_that("LearnerClassifAutoCatboost twoclass internal eval metric is found", {
   skip_on_cran()
+  skip_if_not_installed("catboost")
   skip_if_not_installed("rush")
   flush_redis()
 
-  rush_plan(n_workers = 2)
-  
+  rush_plan(n_workers = 1)
 
   task_twoclass = tsk("pima")
   msrs_twoclass = rbindlist(list(
@@ -55,9 +60,11 @@ test_that("LearnerClassifAutoCatboost twoclass internal eval metric is found", {
       small_data_size = 1,
       resampling = rsmp("holdout"),
       measure = msr(msrs_twoclass$measure[[i]]),
-      terminator = trm("evals", n_evals = 6),
+      terminator = trm("evals", n_evals = 1),
       store_benchmark_result = TRUE,
-      store_models = TRUE
+      store_models = TRUE,
+      encapsulate_learner = FALSE,
+      encapsulate_mbo = FALSE
     )
     learner$train(task_twoclass)
 
@@ -70,11 +77,11 @@ test_that("LearnerClassifAutoCatboost twoclass internal eval metric is found", {
 
 test_that("LearnerClassifAutoCatboost multiclass internal eval metric is found", {
   skip_on_cran()
+  skip_if_not_installed("catboost")
   skip_if_not_installed("rush")
   flush_redis()
 
-  rush_plan(n_workers = 2)
-
+  rush_plan(n_workers = 1)
 
   task_multiclass = tsk("penguins")
   msrs_multiclass = rbindlist(list(
@@ -89,9 +96,11 @@ test_that("LearnerClassifAutoCatboost multiclass internal eval metric is found",
       small_data_size = 1,
       resampling = rsmp("holdout"),
       measure = msr(msrs_multiclass$measure[[i]]),
-      terminator = trm("evals", n_evals = 6),
+      terminator = trm("evals", n_evals = 1),
       store_benchmark_result = TRUE,
-      store_models = TRUE
+      store_models = TRUE,
+      encapsulate_learner = FALSE,
+      encapsulate_mbo = FALSE
     )
     learner$train(task_multiclass)
 
@@ -100,7 +109,7 @@ test_that("LearnerClassifAutoCatboost multiclass internal eval metric is found",
       msrs_multiclass$metric[[i]]
     )
   })
-})  
+})
 
 test_that("catboost not supported internal eval metric throws error", {
   skip_on_cran()
