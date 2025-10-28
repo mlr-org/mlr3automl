4f8be49ed65f3a7bb72d4c84b533a578c82e29b4
Marc Becker <33069354+be-marc@users.noreply.github.com>
2025-10-28 10:41:16 +0100

    feat: add regression learner (#49)
    * feat: add regression learner
    
    * ...

---
+++ R/LearnerRegrAuto.R
@@ -0,0 +1,106 @@
+#' @title Regression AutoML Learner
+#'
+#' @description
+#' The [LearnerRegrAuto] is an automated machine learning (AutoML) system for regression tasks.
+#' It combines preprocessing, a switch between multiple learners and hyperparameter tuning to find the best model for the given task.
+#'
+#' @template param_id
+#' @template param_learner_ids
+#' @template section_debugging
+#' @template section_parameters
+#'
+#' @export
+LearnerRegrAuto = R6Class("LearnerRegrAuto",
+  inherit = Learner,
+  public = list(
+
+    #' @field tuning_space (`list()`).
+    tuning_space = NULL,
+
+    #' @field instance ([mlr3tuning::TuningInstanceAsyncSingleCrit]).
+    instance = NULL,
+
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    initialize = function(
+      id = "regr.auto",
+      learner_ids
+      ) {
+      all_learner_ids = mlr_auto$keys()
+      if (missing(learner_ids)) learner_ids = all_learner_ids
+      assert_subset(learner_ids, all_learner_ids)
+      private$.learner_ids = learner_ids
+
+      param_set = ps(
+        learner_timeout = p_int(lower = 1L, init = 900L, tags = c("train", "super")),
+        # system
+        n_threads = p_int(lower = 1L, init = 1L, tags = c("train", "catboost", "lightgbm", "ranger", "xgboost")),
+        memory_limit = p_int(lower = 1L, init = 32000L, tags = c("train", "catboost", "lightgbm", "ranger", "xgboost")),
+        devices = p_uty(init = "cpu", tags = c("train", "super"), custom_check = crate({function(x) check_subset(x, c("cpu", "cuda"))})),
+        # large data
+        large_data_size = p_int(lower = 1L, init = 1e6, tags = c("train", "super")),
+        # small data
+        small_data_size = p_int(lower = 1L, init = 5000L, tags = c("train", "super")),
+        small_data_resampling = p_uty(init = rsmp("cv", folds = 10), tags = c("train", "super")),
+        # tuner
+        initial_design_size = p_int(lower = 1L, init = 4L, tags = c("train", "super")),
+        initial_design_type = p_uty(init = c("default", "lhs"), tags = c("train", "super"), custom_check = crate({function(x) check_subset(x, c("lhs", "set", "random", "default"))})),
+        resampling = p_uty(init = rsmp("holdout"), tags = c("train", "super")),
+        terminator = p_uty(init = trm("run_time", secs = 3600), tags = c("train", "super")),
+        measure = p_uty(tags = c("train", "super")),
+        callbacks = p_uty(tags = c("train", "super")),
+        store_benchmark_result = p_lgl(init = FALSE, tags = c("train", "super")),
+        store_models = p_lgl(init = FALSE, tags = c("train", "super")),
+        # debugging
+        encapsulate_learner = p_lgl(init = TRUE, tags = c("train", "super")),
+        encapsulate_mbo = p_lgl(init = TRUE, tags = c("train", "super")),
+        check_learners = p_lgl(init = TRUE, tags = c("train", "super"))
+      )
+      # subset to relevant parameters for selected learners
+      param_set = param_set$subset(ids = unique(param_set$ids(any_tags = c("super", learner_ids))))
+
+      autos = mlr_auto$mget(learner_ids)
+      packages = unlist(map(autos, "packages"))
+
+      super$initialize(
+        id = id,
+        task_type = "regr",
+        param_set = param_set,
+        packages = union(c("mlr3", "mlr3tuning","mlr3pipelines", "mlr3learners"), packages),
+        feature_types = c("logical", "integer", "numeric", "character", "factor"),
+        predict_types = "response",
+        properties = c("missings", "weights"),
+      )
+    }
+  ),
+
+  private = list(
+    .learner_ids = NULL,
+
+   .train = function(task) {
+      train_auto(self, private, task)
+    },
+
+    .predict = function(task) {
+      lg$debug("Predicting with '%s' on task '%s'", self$id, task$id)
+      self$model$graph_learner$predict(task)
+    },
+
+    deep_clone = function(name, value) {
+      if (is.R6(value)) {
+        return(value$clone(deep = TRUE))
+      } else if (name == "state") {
+        if (!is.null(value)) {
+          value = list(
+            instance = value$instance$clone(deep = TRUE))
+        }
+        return(value)
+      } else {
+        super$deep_clone(name, value)
+      }
+    }
+  )
+)
+
+#' @include aaa.R
+learners[["regr.auto"]] = LearnerRegrAuto
---
+++ tests/testthat/test_LearnerRegrAuto.R
@@ -0,0 +1,11 @@
+test_that("LearnerRegrAuto is initialized", {
+  learner = lrn("regr.auto",
+    measure = msr("regr.rmse"),
+    terminator = trm("evals", n_evals = 10))
+
+  expect_learner(learner, task = tsk("california_housing"))
+})
+
+test_that("all learner on cpu work", {
+  test_regr_learner(c("catboost", "glmnet", "lightgbm", "ranger", "svm", "xgboost", "extra_trees"), initial_design_type = c("lhs", "default"), n_evals = 30)
+})
