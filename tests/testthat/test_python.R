b753dcd918a8160283cb2a9d7e5f762e076cd4d1
be-marc <marcbecker@posteo.de>
2026-07-16 17:45:36 +0200

    ...

--- tests/testthat/test_python.R
+++ tests/testthat/test_python.R
@@ -1,4 +1,6 @@
 test_that("check_python_packages works", {
+  skip_if_not_installed("reticulate")
+
   res = check_python_packages(c("package_1", "package_2"))
   expect_equal(res, "Package package_1,package_2 not available.")
 
