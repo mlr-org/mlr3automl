1732cad7458df6350d4b402a419f17953e570d40
be-marc <marcbecker@posteo.de>
2026-07-17 12:43:19 +0200

    test: error in no tuning result

--- tests/testthat/test_train_auto.R
+++ tests/testthat/test_train_auto.R
@@ -1,6 +1,43 @@
+test_that("training errors when all evaluations fail", {
+  skip_on_cran()
+  skip_if_not_installed("rush")
+  # the surrogate model of the mbo tuner requires ranger
+  skip_if_not_installed("ranger")
+  skip_if_no_redis()
+
+  rush = start_rush()
+  on.exit({
+    rush$reset()
+    mirai::daemons(0)
+  })
+
+  mlr_auto$add("debug", function() AutoDebug$new(error_train = 1))
+  on.exit(mlr_auto$add("debug", function() AutoDebug$new()), add = TRUE)
+
+  task = tsk("penguins")
+  learner = lrn(
+    "classif.auto",
+    learner_ids = "debug",
+    rush = rush,
+    small_data_size = 1,
+    resampling = rsmp("holdout"),
+    measure = msr("classif.ce"),
+    terminator = trm("evals", n_evals = 2),
+    initial_design_type = "random",
+    initial_design_size = 2,
+    encapsulate_learner = FALSE,
+    encapsulate_mbo = FALSE
+  )
+
+  # bbotk errors with a plain `Mlr3Error` because no evaluation finished
+  expect_error(learner$train(task), class = "Mlr3Error")
+})
+
 test_that("failed final model fit does not silently return a featureless model", {
   skip_on_cran()
   skip_if_not_installed("rush")
+  # the surrogate model of the mbo tuner requires ranger
+  skip_if_not_installed("ranger")
   skip_if_no_redis()
 
   rush = start_rush()
@@ -34,6 +71,8 @@ test_that("failed final model fit does not silently return a featureless model",
 test_that("encapsulated auto learner falls back on a failed final model fit", {
   skip_on_cran()
   skip_if_not_installed("rush")
+  # the surrogate model of the mbo tuner requires ranger
+  skip_if_not_installed("ranger")
   skip_if_no_redis()
 
   rush = start_rush()
